c9f4c8741f96f52d288b8e96b80562197ef64493
김주경 <kimjugyeong@gimjugyeong-ui-MacBookPro.local>
2022-01-03 00:26:04 +0900

    blogJoo project ok

---
+++ blogJoo/front/components/AppLayout.js
@@ -0,0 +1,9 @@
+import PropTypes from "prop-types";
+
+const AppLayout = props => {
+  return <div>AppLayout</div>;
+};
+
+AppLayout.propTypes = {};
+
+export default AppLayout;
---
+++ blogJoo/front/pages/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function index() {
+  return <div>index.js</div>;
+}
+
+export default index;
